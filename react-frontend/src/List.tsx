5ebc8fc783a06ae74287831f5f08558ae530f3ad
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2026-07-29 11:18:03 +0200

    Aufgabe 1 Lösung

--- react-frontend/src/List.tsx
+++ react-frontend/src/List.tsx
@@ -1,5 +1,45 @@
+import { type ReactNode } from "react";
+
+type Movie = {
+    id: string;
+    title: string;
+    year: number;
+};
+
+const movies: Movie[] = [
+    { id: '1', title: 'The Shawshank Redemption', year: 1994 },
+    { id: '2', title: 'The Godfather', year: 1972 },
+    { id: '3', title: 'The Dark Knight', year: 2008 },
+    { id: '4', title: 'Pulp Fiction', year: 1994 },
+    { id: '5', title: 'The Lord of the Rings: The Return of the King', year: 2003 },
+];
+
+
 const List: React.FC = () => {
-    return 'List works'
+    // if (movies.length === 0) {
+    //     return <div>Keine Daten vorhanden</div>
+    // }
+
+    let tableContent: ReactNode = <tr><td colSpan={2}>Keine Daten vorhanden</td></tr>;
+
+    if (movies.length > 0) {
+        tableContent = movies.map(movie => <tr>
+            <td>{movie.title}</td>
+            <td>{movie.year}</td>
+        </tr>);
+    }
+
+    return <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Year</th>
+            </tr>
+        </thead>
+        <tbody>
+            {tableContent}
+        </tbody>
+    </table>
 }
 
 export default List;
