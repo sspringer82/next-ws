083127aa01ebb62633fc012b15d26bfb73b6fc8b
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2026-07-29 13:53:42 +0200

    Aufgabe 2 Lösung

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import List from "./List";
 import StateLifecycle from "./StateLifecycle";
-
 const App: React.FC = () => {
-    return <StateLifecycle />;
+    const [state, setState] = useState(true)
+
+    return <div>
+        <button onClick={() => setState(false)}>hide</button>
+        {state && <List />}
+    </div>
 }
 export default App;
--- react-frontend/src/List.tsx
+++ react-frontend/src/List.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 
 type Movie = {
     id: string;
@@ -6,7 +6,7 @@ type Movie = {
     year: number;
 };
 
-const movies: Movie[] = [
+const moviesData: Movie[] = [
     { id: '1', title: 'The Shawshank Redemption', year: 1994 },
     { id: '2', title: 'The Godfather', year: 1972 },
     { id: '3', title: 'The Dark Knight', year: 2008 },
@@ -14,16 +14,35 @@ const movies: Movie[] = [
     { id: '5', title: 'The Lord of the Rings: The Return of the King', year: 2003 },
 ];
 
-
 const List: React.FC = () => {
     // if (movies.length === 0) {
     //     return <div>Keine Daten vorhanden</div>
     // }
 
+    const [movies, setMovies] = useState<Movie[]>([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setMovies(moviesData)
+        }, 1000);
+        const ival = setInterval(() => {
+            console.log('do horrible things')
+        }, 1000);
+
+        return () => {
+            clearInterval(ival);
+            console.log('UNMOUNT!!!')
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log('render + update');
+    })
+
     let tableContent: ReactNode = <tr><td colSpan={2}>Keine Daten vorhanden</td></tr>;
 
     if (movies.length > 0) {
-        tableContent = movies.map(movie => <tr>
+        tableContent = movies.map(movie => <tr key={movie.id}>
             <td>{movie.title}</td>
             <td>{movie.year}</td>
         </tr>);
