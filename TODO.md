d3f0c5f5b5c75d42682085a01bb5be01132cf1cd
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2026-07-30 16:56:10 +0200

    Agenda

---
+++ TODO.md
@@ -0,0 +1,17 @@
+# todo
+
+- SSG
+- styling
+- Siemens ix
+- api routen + Security
+- filtern mit searchParams
+- Kombination Client/Server
+- Server Functions + revalidate (DELETE)
+- Form handling
+- Loading.tsx, Error.tsx, Notfound.tsx
+
+- echtes Streaming
+- Streaming
+- Testing
+- Charting
+- *ing
--- next-app/src/components/list/list-item.tsx
+++ next-app/src/components/list/list-item.tsx
@@ -9,7 +9,9 @@ const ListItem: React.FC<Props> = ({ movie }) => {
     return <tr>
         <td>{movie.title}</td>
         <td>{movie.year}</td>
-        <td><Link href={`/movies/${movie.id}`}>Details</Link></td>
+        <td>
+            <Link href={`/movies/${movie.id}`}>Details</Link>
+        </td>
     </tr>
 }
 
